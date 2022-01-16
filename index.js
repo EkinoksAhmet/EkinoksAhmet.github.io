8dab665fd4e6b1622bceaa0b5a80c03439d3c69f
EkinoksAhmet <80361129+EkinoksAhmet@users.noreply.github.com>
2022-01-17 00:46:12 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('menuButton').addEventListener("click", () => {
-    if (document.getElementById('responsiveMenu').style.display === 'block') {
-        document.getElementById('responsiveMenu').style.display = 'none';
-    } else if (document.getElementById('responsiveMenu').style.display === 'none') {
-        document.getElementById('responsiveMenu').style.display = 'block';
-    }
-});
